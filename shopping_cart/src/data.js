e7416eaf3d887f5977e82bfeaae27041225b85d3
Elif <60880193+Elif-web@users.noreply.github.com>
2022-06-13 15:38:48 +0300

    Add files via upload
    first commit

---
+++ shopping_cart/src/data.js
@@ -0,0 +1,84 @@
+let shopItemsData = [{
+    id:"one",
+    name:"White Shirt",
+    price: 65,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/pexels-dziana-hasanbekava-7626615.jpg"
+},
+{
+    id:"two",
+    name:"Shoulder Bag",
+    price: 40,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/img1.jpg"
+},
+{
+    id:"three",
+    name:"High Heels Shoes",
+    price: 100,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/pexels-nara-meas-3598123.jpg"
+},
+{
+    id:"four",
+    name:"Ruffle Skirt",
+    price: 75,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/pexels-quang-anh-ha-nguyen-884979.jpg"
+},
+{
+    id:"five",
+    name:"Long Dress",
+    price: 95,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/4.jpg"
+},
+{
+    id:"six",
+    name:"Pink Hat",
+    price: 40,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/45.jpg"
+},
+{
+    id:"seven",
+    name:"Patterned Kimono",
+    price: 45,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/65.jpg"
+},
+{
+    id:"eight",
+    name:"Tassle Shirt",
+    price: 55,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/87.jpg"
+},
+{
+    id:"nine",
+    name:"Pink Dress",
+    price: 70,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/1.jpg"
+},
+{
+    id:"ten",
+    name:"Stiletto Shoes",
+    price: 50,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/8.jpg"
+},
+{
+    id:"eleven",
+    name:"Lace Shirt",
+    price: 55,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/3.jpg"
+},
+{
+    id:"twelve",
+    name:"Pearl Earrings",
+    price: 30,
+    desc: "lorem ipsum dolor sit amet",
+    img: "images-/7.jpg"
+}];
